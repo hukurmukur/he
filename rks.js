7181ad1fe0a12680405309df417dd39de4b83870
hukurmukur <165541026+hukurmukur@users.noreply.github.com>
2024-03-31 11:03:26 +0530

    Add files via upload

---
+++ rks.js
@@ -0,0 +1,5 @@
+document.getElementById('toggleButton').addEventListener('click', function() {
+    var paragraph = document.querySelector('.large-paragraph');
+    paragraph.classList.toggle('hide');
+  });
+  
